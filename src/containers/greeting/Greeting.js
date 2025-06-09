afe03ff0241cfa3af95cbf68b55e933ec74cf3f7
parthsi17 <parth.singh564@gmail.com>
2025-06-10 01:12:10 +0530

    Update Greeting.js

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -42,7 +42,7 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume1.pdf")}
+                    href={require("./SDE -Resume-Parth Singh.pdf")}
                     download="Resume.pdf"
                     className="download-link-button"
                   >
