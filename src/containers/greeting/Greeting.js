13d33e938c70ab1a3e4c0acb4756995da6e7caf1
Parth Singh <parthsingh@naraci.com>
2025-06-12 12:06:06 +0530

    Resume Edits

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -42,7 +42,7 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./SDE -Resume-Parth Singh.pdf")}
+                    href={require("./Parth-Singh-SDE-Resume.pdf")}
                     download="Resume.pdf"
                     className="download-link-button"
                   >
